3d94eb49540bd804c7bcc9b356380cdd19c9870a
SupawitKaennak <SupawitKaennak@hotmail.com>
2025-08-03 15:40:40 +0700

    edit title layout

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css"
 import { MaterialProvider } from "@/components/material-provider"
 
 export const metadata: Metadata = {
-  title: "Income Expense Tracker",
-  description: "Track your income and expenses",
+  title: "RMUTL_PATONPHUENG_LEAF_PLATE_MANAGEMENT",
+  description: "Track income and expenses",
     generator: 'v0.dev'
 }
 
